10e55503dab62a50db8fc146db378d4684199899
Mohammed Alhamadani <68984375+mohamedbasil@users.noreply.github.com>
2024-03-22 01:43:30 -0400

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,30 @@
+
+let count = 0;
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+
+// console.log(countEl)
+
+function increment() {
+    count = count + 1;
+    countEl.innerHTML = count
+    console.log(count);
+    
+}
+
+function save() { 
+
+    let countStr=count+"-"
+    saveEl.innerText += countStr
+    countEl.innerText = 0
+    count = 0
+    
+    
+    
+
+
+
+}
+
